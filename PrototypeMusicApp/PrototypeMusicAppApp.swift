d1873f931e6145fc32213a2daf76114b0ac4b56c
codenamenam <bluetire27@gmail.com>
2021-06-30 08:08:31 +0900

    Initial Commit

---
+++ PrototypeMusicApp/PrototypeMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototypeMusicAppApp.swift
+//  PrototypeMusicApp
+//
+//  Created by nam on 2021/06/30.
+//
+
+import SwiftUI
+
+@main
+struct PrototypeMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
